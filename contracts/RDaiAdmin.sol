cce4a4e9fdcf873c3e8c37b534be9b5861762be7
Andrew Redden <andrew@groundhog.network>
2019-11-04 21:00:04 -0400

    adjustments to arguments for the authP method, and some tweaks to help compiling

--- contracts/RDaiAdmin.sol
+++ contracts/RDaiAdmin.sol
@@ -24,16 +24,18 @@ contract RDaiAdmin is IForwarder, AragonApp {
     bytes32 public constant CHANGE_HAT = keccak256("CHANGE_HAT");
     bytes32 public constant EXECUTE_VOTE = keccak256("EXECUTE_VOTE");
     bytes32 public constant CREATE_VOTE = keccak256("CREATE_VOTE");
+    bytes32 public constant ADD_TOKEN = keccak256("ADD_TOKEN");
+    bytes32 public constant REMOVE_TOKEN = keccak256("REMOVE_TOKEN"); // #TODO
 
 
     function forward(
         bytes _evmScript
     )
-    public
+        public
     {
         require(canForward(msg.sender, _evmScript));
         uint256 voteId = voting.newVote(
-            _executionScript,
+            _evmScript,
             "",
             true,
             false
@@ -45,10 +47,9 @@ contract RDaiAdmin is IForwarder, AragonApp {
         address _sender,
         bytes _evmCallScript
     )
-    public
-    view
-    returns
-    (bool)
+        public
+        view
+        returns (bool)
     {
         return canPerform(
             _sender,
@@ -58,20 +59,29 @@ contract RDaiAdmin is IForwarder, AragonApp {
     }
 
     function isForwarder()
-    public
-    pure
-    returns (bool)
+        public
+        pure
+        returns (bool)
     {
         return true;
     }
 
+    function _setToken(
+        bytes32 _tokenType,
+        address _tokenAddress
+    )
+        internal
+    {
+        rTokens[_tokenType] = _tokenAddress;
+        emit AddToken(_tokenType, _tokenAddress);
+    }
     // do we need to setup the agent before hand or should the dao own the creation of these?
     /// @dev rToken contract address
     function initialize(
         address _agent,
         address _voting,
-        string [] _rTokenType,
-        address[] _rTokenAddress
+        string _rTokenType,
+        address _rTokenAddress
     )
         public
         onlyInit
@@ -80,24 +90,25 @@ contract RDaiAdmin is IForwarder, AragonApp {
         initialized();
         // setup agents
         agent = Agent(_agent);
+        emit NewAgentSet(address(_agent));
         voting = Voting(_voting);
-        emit NewAgentSet(_agent);
+        emit NewVotingSet((address(voting)));
         //setup rTokens
-        for(uint i = 0; i < _rTokenType.length; i++)
-        {
-            _addToken(keccak256(_rTokenType[i]), _rTokenAddress[i]);
-        }
+        _setToken(
+            _rTokenType,
+            _rTokenAddress
+        );
     }
+
 
     function addToken(
         bytes32 _tokenType,
         address _tokenAddress
     )
-        external
-        authP(ADD_TOKEN, arr(_tokenType, _tokenAddress))
+        public
+//        authP(ADD_TOKEN, arr(_tokenAddress, uint256(_tokenType)))
     {
-        rTokens[_tokenType] = _tokenAddress;
-        emit AddToken(_tokenType, _tokenAddress);
+        _setToken(_tokenType, _tokenAddress);
     }
 
 
@@ -107,8 +118,8 @@ contract RDaiAdmin is IForwarder, AragonApp {
         address _target,
         uint256 _hatId
     )
-        external
-        authP(CHANGE_HAT, arr(_rToken, _target, _hatId))
+        public
+        authP(CHANGE_HAT, arr(_target, _hatId, uint256(_rToken)))
     {
         //verify there is contract data at the target
         string memory functionSignature = "changeHatFor(address, uint256)";
